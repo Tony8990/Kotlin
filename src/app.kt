1137f92b2a62d56b218de6970a7dfca8e7f27113
Unknown <tony.fava@edu.itspiemonte.it>
2018-04-09 14:37:17 +0200

    asdsadasdsa

--- src/app.kt
+++ src/app.kt
@@ -1,22 +1,12 @@
 
-import java.io.IOException
-import java.nio.file.Files
-import java.nio.file.Path
+
 fun main(args:Array<String>){
- fun readFile(path:Path){
-    val input = Files.newInputStream(path)
-    try {
-        var byte= input.read()
-        while (byte != 1)
-        {
-            println(byte)
-            byte= input.read()
-        }
-    }catch (e:IOException){
-        println("Error reading ${e.message}")
-    }finally {
-        input.close()
+
+val list = listOf(1,2,3,4,5)
+    for (k in list)
+    {
+        println(k)
     }
- }
+
 }
 
